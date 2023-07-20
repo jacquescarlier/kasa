23a6739f14dff43f40640fe887dbd2237f9175a9
jacques <jacques.carlier@gmail.com>
2023-07-20 13:28:44 +0200

    start page 404

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { NavLink } from "react-router-dom";
 
 const Page404 = () => {
   return (
     <div>
-      <main>
+      <container-general>
         <Header />
         <div className="page404">
         <img src="404.png" alt="" />
         <h1>Oups! La page que vous demandez n'existe pas.</h1>
+        <NavLink to="/"><p className="goHome">Retourner sur la page d'accueil</p></NavLink>
         </div>
-
-      </main>
+      </container-general>
       <Footer />
     </div>
   );

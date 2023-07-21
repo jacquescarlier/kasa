be45fc9822a0791acecedb9f709eb798c6dc0810
jacques <jacques.carlier@gmail.com>
2023-07-21 20:21:05 +0200

    git problem

--- src/components/HousingCarousel.js
+++ src/components/HousingCarousel.js
@@ -1,9 +1,48 @@
-import React, { useState } from "react";
-import ArrowRight from "../pictures/arrow_back.png";
-import ArrowLeft from "../pictures/arrow_forward.png";
+import React, { useState } from "react"
+import ArrowRight from "../pictures/arrow_forward.png"
+import ArrowLeft from "../pictures/arrow_back.png"
 
-const HousingCarousel = () => {
-  return <div></div>;
+import { useParams } from "react-router-dom"
+import logements from '../datas/logements.json'
+
+const useHousing = () => {
+    const { id } = useParams();
+    const logement = logements.find((item) => item.id === id)
+    return logement;
 };
 
-export default HousingCarousel;
+
+function Carrousel() {
+    const logement = useHousing();
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const pictureCounter = `${currentImageIndex + 1} / ${logement.pictures.length}`
+    const nextImage = () => {
+        setCurrentImageIndex((nextIndex) => (nextIndex + 1) % logement.pictures.length)
+    };
+    const previousImage = () => {
+        setCurrentImageIndex((prevIndex) => {
+            if (prevIndex === 0) {
+                return logement.pictures.length - 1;
+            } else {
+                return (prevIndex - 1) % logement.pictures.length;
+            }
+        });
+    }
+    const currentImage = logement.pictures[currentImageIndex]
+    if (logement.pictures.length === 1) {
+        return (
+            <section className="carrousel">
+                <img src={currentImage} alt="logement" className="carrouselImg" />
+            </section>
+        );
+    }
+    return (
+        <section className="carrousel">
+            <img src={currentImage} alt="logement" className="carrouselImg" />
+            <img src={ArrowRight} alt="Fléche droite " onClick={nextImage} className="carrouselArrow carrouselArrowRight "></img>
+            <img src={ArrowLeft} alt='Fléche gauche' onClick={previousImage} className="carrouselArrow carrouselArrowLeft"></img>
+            <div className="carrouselCounter"> {pictureCounter}</div>
+        </section>
+    )
+}
+export default Carrousel
--- src/pages/FicheLogement.js
+++ src/pages/FicheLogement.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-{/*import logements from "../datas/logements.json";*/}
-{/*import HousingCarousel from "../components/HousingCarousel";
-import HousingDetail from "../components/HousingDetail";*/}
+import HousingCarousel from "../components/HousingCarousel"
+
+
 
 const FicheLogement = () => {
   return (
     <div>
       <container-general>
         <Header />
-        
+      <HousingCarousel />
       </container-general>
       <Footer />
     </div>

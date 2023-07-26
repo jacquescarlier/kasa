6caa386e58f0f453c4f24965d2e7bf451e207a4f
jacques <jacques.carlier@gmail.com>
2023-07-26 08:17:26 +0200

    fixed dropdown.js file while waiting for 'accordion.js' modification

---
+++ src/components/Dropdown.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Arrow from "../pictures/arrow_down.png";
+
+function Dropdown({ children, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isRotated, setIsRotated] = useState(false);
+  const handleClick = () => {
+    setIsRotated(!isRotated);
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className="dropdown">
+      <div className="dropdownTitle">
+        <p className="dropdownText">{text}</p>
+        <img
+          onClick={handleClick}
+          src={Arrow}
+          alt="Fléche pour afficher la description"
+          className={isRotated ? "rotated" : "arrow"}
+        />
+      </div>
+      <div className={`dropdownContent ${isOpen ? "open" : "close"}`}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;

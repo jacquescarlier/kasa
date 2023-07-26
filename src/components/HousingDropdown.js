0f1be3982023def053ed73c33d709b56ccb76a19
jacques <jacques.carlier@gmail.com>
2023-07-26 14:01:56 +0200

    delete useNavigate

--- src/components/HousingDropdown.js
+++ src/components/HousingDropdown.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { useEffect } from 'react';
 import logements from '../datas/logements.json'
-import { useParams, useNavigate, } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import Accordion from './Accordion';
 
 const title = ["Description", "Équipements"]
@@ -9,16 +8,7 @@ const title = ["Description", "Équipements"]
 const HousingDropdown = () => {
     const { id } = useParams()
     const logement = logements.find((housing) => housing.id === id)
-    const navigate = useNavigate();
-
-    useEffect(() => {
-
-        if (!logement.id) {
-            console.log("Logement non trouvé.404");
-            navigate("/Page404");
-        }
-    }, [logement.id, navigate]);
-
+   
     return (
         <section className="accordionSection ">
             <div className="accordionBody">

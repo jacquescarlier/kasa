04004fc865d0aeb7ad0fcd33ce2a0ec898bc65ed
jacques <jacques.carlier@gmail.com>
2023-07-27 18:12:14 +0200

    backup

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 function Banner({ image, title, alt }) {
+ 
   return (
     <section className="image-banner">
       <img src={image} alt={alt} />
-      <h1 className="banner-text">{title}</h1>
+      <h1 className="banner-text"><p>Chez vous,</p><p>partout et ailleurs</p></h1>
     </section>
   );
 }
